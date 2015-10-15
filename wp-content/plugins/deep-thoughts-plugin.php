f2462ddfcaeac260735dd011b66808ee147c200d
Wenson Tsai <wensontsai@gmail.com>
2015-10-15 17:30:04 -0400

    plugin

---
+++ wp-content/plugins/deep-thoughts-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Deep Thoughts Functionality
+Description: API Modifications for my Deep Thoughts React Native app.
+Author: Jeffrey Gould
+Version: 1.0
+Author URI: http://jrgould.com
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
